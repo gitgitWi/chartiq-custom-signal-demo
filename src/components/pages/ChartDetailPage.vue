7a7b4432e7ec95b416c71eeceffbd4a756c2bf28
gitgitWi <wiii@kakao.com>
2022-06-15 13:49:11 +0900

    Feat (chart-page) 차트 페이지 구현

---
+++ src/components/pages/ChartDetailPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ChartContainer from '../organisms/chart-container/ChartContainer.vue';
+</script>
+
+<template>
+  <h1>Chart Detail Page</h1>
+  <chart-container></chart-container>
+</template>
+
+<style lang="scss" scoped>
+@import '../../styles';
+
+h1 {
+  color: rgba(40, 40, 40, 0.8);
+  text-align: center;
+}
+</style>
