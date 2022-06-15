f19b34027cd783c21cc258dd7893a6db0170b0d6
gitgitWi <wiii@kakao.com>
2022-06-15 13:49:45 +0900

    Feat (App) 엔트리 컴포넌트에 chart-detail 페이지 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import ChartDetailPage from './components/pages/ChartDetailPage.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <chart-detail-page id="chart-detail-page" />
 </template>
 
-<style>
+<style lang="scss">
+@import './styles';
+
+#chart-detail-page {
+  @extend .reset-box;
+  width: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
