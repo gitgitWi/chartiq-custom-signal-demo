69abd014dbd37c74372669e71c8a40632c40f5cb
gitgitWi <wiii@kakao.com>
2022-06-15 13:45:21 +0900

    Feat (main.ts) ChartIQ custom element 컴포넌트로 등록

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+/**
+ * ChartIQ custom element 등록
+ */
+app.config.compilerOptions.isCustomElement = (tag) => /^cq-.*$/.test(tag);
+app.component('TemplatePlaceholder', {
+  render(createElement: any) {
+    console.warn(createElement);
+    return createElement('template', this.$slots.default);
+  },
+});
+
+app.mount('#app');
